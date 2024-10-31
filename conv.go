2839671653b6d7611059426d1a462d946a09adcf
VolkovVI <volkowladislav@gmail.com>
2024-10-31 12:47:41 +0300

    5 lesson func Itoa

---
+++ conv.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var test int
+var fnh string
+
+func cnv(str int) string {
+	return strconv.Itoa(str)
+}
+
+func main() {
+
+	test = -8
+	fnh = cnv(test)
+	fmt.Print(fnh)
+
+}
